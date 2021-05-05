5222e2b4308b5dca9fc59e985bfbec79995586e4
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2021-05-06 02:14:40 +0530

    pages: add base home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Card, Typography, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    margin: `${theme.spacing(5)}px 30px`,
+  },
+  title: {
+    padding: `${theme.spacing(2)}px`,
+    color: theme.palette.text.secondary,
+    fontSize: '1em',
+  },
+}))
 
 function Home(props) {
-  return <div></div>
+  const classes = useStyles()
+  
+  return (
+    <Card className={classes.card}>
+      <Typography variant='h2' className={classes.title}>
+        Popular Videos
+      </Typography>
+    </Card>
+  )
 }
 
 export default Home
