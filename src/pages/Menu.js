178ca588c780b7d91bc0e655f4e1af129bfd42cf
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2021-05-06 02:14:17 +0530

    pages: add base appbar

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home'
+import { Link } from 'react-router-dom'
 
-function Menu(props) {
-  return <div></div>
+const Menu = () => {
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <div>
+          <Link to='/'>
+            <IconButton>
+              <HomeIcon color='secondary' />
+            </IconButton>
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Menu
