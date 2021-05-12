f462a09c72e16ab7b92235ceef76613589ab21b8
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2021-05-12 17:22:34 +0530

    src: reorganize PrivateRoute component

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
-import authHelper from './authHelper'
+import authHelper from './utils/authHelper'
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
