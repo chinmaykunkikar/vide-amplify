a16190d3d247fc4b69f9b52505e5ad1b32c9afc9
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2021-05-12 20:27:32 +0530

    src: update Appbar logic to reflect newly introduced pages and components

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
 import Menu from './pages/Menu'
 import Home from './pages/Home'
+import Signin from './pages/Signin'
+import NewVideo from './pages/NewVideo'
 
 const MainRouter = ({ data }) => {
   return (
@@ -9,6 +12,8 @@ const MainRouter = ({ data }) => {
       <Menu />
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/signin' component={Signin} />
+        <PrivateRoute path='/videos/new' component={NewVideo} />
       </Switch>
     </div>
   )
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import {
-  AppBar,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from '@material-ui/core'
-import HomeIcon from '@material-ui/icons/Home'
+import { AppBar, Button, IconButton, Toolbar } from '@material-ui/core'
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined'
+import VideoCallOutlinedIcon from '@material-ui/icons/VideoCallOutlined'
 import { Link } from 'react-router-dom'
+import authHelper from '../utils/authHelper'
+import Signout from '../components/Signout'
 
 const Menu = () => {
   return (
@@ -16,9 +13,34 @@ const Menu = () => {
         <div>
           <Link to='/'>
             <IconButton>
-              <HomeIcon color='secondary' />
+              <HomeOutlinedIcon color='secondary' />
             </IconButton>
           </Link>
+        </div>
+        <div style={{ position: 'absolute', right: '12px' }}>
+          <span style={{ float: 'right' }}>
+            {!authHelper.isAuthenticated() && (
+              <Link to='/signin'>
+                <Button color='secondary' variant='outlined'>
+                  Sign In
+                </Button>
+              </Link>
+            )}
+            {authHelper.isAuthenticated() && (
+              <span>
+                <Link to='/videos/new'>
+                  <Button
+                    color='secondary'
+                    variant='outlined'
+                    style={{ margin: '8px' }}>
+                    <VideoCallOutlinedIcon style={{ marginRight: '8px' }} />
+                    New Video
+                  </Button>
+                </Link>
+                <Signout color='secondary' variant='outlined' />
+              </span>
+            )}
+          </span>
         </div>
       </Toolbar>
     </AppBar>
