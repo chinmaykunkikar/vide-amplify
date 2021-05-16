9012a1445c9f1017154424598af5463303ec1924
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2021-05-16 15:51:37 +0530

    authHelper: hacks

--- src/utils/authHelper.js
+++ src/utils/authHelper.js
@@ -1,11 +1,9 @@
-import { Auth } from 'aws-amplify'
-
 const authHelper = {
   isAuthenticated() {
-    if (typeof window == 'undefined') return false
-    if (Auth.currentSession().then(authData => authData.idToken))
-      return Auth.currentSession().then(authData => authData.idToken)
-    else return false
+    return (
+      Object.keys(localStorage).filter(key => key.endsWith('userData'))
+        .length !== 0
+    )
   },
 }
 
