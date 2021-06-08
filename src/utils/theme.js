6ebd4a74a0c9b72eb01e732db16cc580389c355d
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2021-06-08 16:32:37 +0530

    theme: define useWidth() hook

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,5 +1,23 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createMuiTheme, useTheme } from '@material-ui/core/styles'
 import { purple, yellow } from '@material-ui/core/colors'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
+/**
+ * Be careful using this hook. It only works because the number of
+ * breakpoints in theme is static. It will break once you change the number of
+ * breakpoints. See https://reactjs.org/docs/hooks-rules.html#only-call-hooks-at-the-top-level
+ */
+export const useWidth = () => {
+  const theme = useTheme()
+  const keys = [...theme.breakpoints.keys].reverse()
+  return (
+    keys.reduce((output, key) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const matches = useMediaQuery(theme.breakpoints.up(key))
+      return !output && matches ? key : output
+    }, null) || 'xs'
+  )
+}
 
 const theme = createMuiTheme({
   palette: {
