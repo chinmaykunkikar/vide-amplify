7f096e181749e977d4272148ee7f2b46bf2e8cf9
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2021-06-17 16:22:54 +0530

    Footer: add repo link to Footer (#21)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,33 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
+import {
+  AppBar,
+  IconButton,
+  Link,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@material-ui/core'
+import { GitHub } from '@material-ui/icons'
 
 const Footer = () => {
   return (
     <AppBar component='footer' position='relative' color='secondary'>
       <Toolbar>
-        <Typography variant='overline' color='inherit'>
-          © 2021 Chinmay
+        <Typography
+          variant='overline'
+          color='inherit'
+          style={{ userSelect: 'none' }}>
+          Chinmay Kunkikar
         </Typography>
+        <Tooltip title='Check out the source code'>
+          <Link
+            target='blank'
+            href='https://github.com/chinmaykunkikar/vide-amplify'>
+            <IconButton>
+              <GitHub color='primary' />
+            </IconButton>
+          </Link>
+        </Tooltip>
       </Toolbar>
     </AppBar>
   )
