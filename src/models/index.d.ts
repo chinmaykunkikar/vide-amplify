f4a0e01507a34c69fe91fc530f0ed3647efff1bf
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-03-26 13:33:06 +0530

    vide: update to amplify Looks like the new amplify process eliminates the need of offline files

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -4,27 +4,35 @@ import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplif
 
 
 
+type UserMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+type VideoMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
 export declare class User {
   readonly id: string;
   readonly name: string;
   readonly email: string;
-  readonly Videos?: (Video | null)[];
-  readonly createdAt?: string;
-  readonly updatedAt?: string;
-  constructor(init: ModelInit<User>);
-  static copyOf(source: User, mutator: (draft: MutableModel<User>) => MutableModel<User> | void): User;
+  readonly Videos?: (Video | null)[] | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+  constructor(init: ModelInit<User, UserMetaData>);
+  static copyOf(source: User, mutator: (draft: MutableModel<User, UserMetaData>) => MutableModel<User, UserMetaData> | void): User;
 }
 
 export declare class Video {
   readonly id: string;
   readonly title: string;
   readonly author: string;
-  readonly description?: string;
-  readonly resourceURI?: string;
-  readonly thumbnailURI?: string;
-  readonly username?: string;
-  readonly createdAt?: string;
-  readonly updatedAt?: string;
-  constructor(init: ModelInit<Video>);
-  static copyOf(source: Video, mutator: (draft: MutableModel<Video>) => MutableModel<Video> | void): Video;
+  readonly description?: string | null;
+  readonly resourceURI?: string | null;
+  readonly thumbnailURI?: string | null;
+  readonly username?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+  constructor(init: ModelInit<Video, VideoMetaData>);
+  static copyOf(source: Video, mutator: (draft: MutableModel<Video, VideoMetaData>) => MutableModel<Video, VideoMetaData> | void): Video;
 }
